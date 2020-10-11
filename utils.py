c96fc08c2e4820e5ae78c81a7b7a9f43b0331b33
LulzLoL231 <lznet@pm.me>
2020-10-11 17:16:57 +0900

    Rewrite log, getServerInfo and getFastConnectURI funcs

--- utils.py
+++ utils.py
@@ -1,37 +1,64 @@
 # -*- coding: utf-8 -*-
 # SourceSeeker – utilites
 # Author: LulzLoL231
-import builtins
-import logging
-from datetime import datetime
+from logging import getLogger, getLevelName, basicConfig, INFO, DEBUG
+from a2s.info import ainfo, SourceInfo
+from typing import Union
 
-import config
+from config import Config
 
 
-def log(func, message, type=None):
-    '''Make new record in log file, or stdout.
+def log(msg: str, level: str = 'INFO', func: Union[str, None] = None) -> None:
+    '''log: make log message.
 
     Args:
-        func   : Log function name.
-        message: Log message.
-        type   : Message type.
+        msg (str): Log message.
+        level (str): Log level. Defaults to 'INFO'.
+        func (Union[str, None]): Function name. Defaults to None.
+    '''
+    basicConfig(level=DEBUG if Config.DEBUG else INFO,
+                format='[%(asctime)s] %(levelname)s | %(name)s | %(message)s')
+    log = getLogger('SourceSeeker')
+    if func:
+        log = getLogger(f'SourceSeeker::{func}')
+    log.log(getLevelName(level.upper()), msg)
+
+
+async def getServerInfo(address: tuple) -> Union[SourceInfo, None]:
+    '''getServerInfo: returns SourceInfo about SourceServer or None if connection unsuccessfull.
+
+    Args:
+        address (tuple): server ip & port in tuple.
 
     Returns:
-        True. Everytime.'''
-    template_stdin = '[ {} ] | {} | func: {} | {}'
-    if config.debug is True:
-        if type is None:
-            type = 'INFO'
-        builtins.print(template_stdin.format(str(datetime.now()), type.upper(), func, message))
-        return True
-    else:
-        logger = logging.getLogger("SourceSeeker")
-        logger.setLevel(config.logging_level)
-        fh = logging.FileHandler("sourceseeker.log")
-        form = logging.Formatter('[ %(asctime)s ] | %(levelname)s | func: %(message)s')
-        fh.setFormatter(form)
-        logger.addHandler(fh)
-        if type is None:
-            type = 'info'
-        eval(f'logger.{type}("\'{func}\' | {message}")')
-        return True
+        SourceInfo: Information about SourceServer.
+    '''
+    info = None
+    iter = 0
+    while True:
+        try:
+            info = await ainfo(address)
+        except Exception as e:
+            log('Error while trying get ServerInfo from '
+                f'{address[0]}:{address[1]} with error: {str(e)}',
+                'error', 'getServerInfo')
+            if iter >= 5:
+                return None
+            else:
+                iter += 1
+                continue
+        else:
+            break
+    return info
+
+
+def getFastConnectURI(address: tuple) -> str:
+    '''getFastConnectURI: returns steam fast connect URI in format `steam://connect/IP:PORT`.
+
+    Args:
+        address (tuple): server ip & port in tuple.
+
+    Returns:
+        str: steam fast connect URI
+    '''
+    return f'steam://connect/{address[0]}/{address[1]}'
