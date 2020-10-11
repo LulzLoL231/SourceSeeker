d67615bff694b8ead9d9cfa9db2474f1954c102e
LulzLoL231 <lznet@pm.me>
2020-10-11 17:26:01 +0900

    Update bot config

---
+++ config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+#  SourceSeeker - config
+#  Created by LulzLoL231 at 11/10/2020
+#
+class Config:
+    '''SourceSeeker config class.
+
+    Contains:
+        TOKEN (str): Discord bot API token.
+        ADMIN (int): Bot admin id.
+        DEBUG (bool): Bot debug.
+    '''
+    TOKEN = ''
+    ADMIN = 0
+    DEBUG = False
